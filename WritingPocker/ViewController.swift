ad253d9a5bb1f56f1216579e09c13566ee1c8c0e
macbook <oreshetchenko@ukr.net>
2019-11-04 22:27:56 +0200

    Initial Commit

---
+++ WritingPocker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WritingPocker
+//
+//  Created by MacBook on 11/4/19.
+//  Copyright © 2019 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
